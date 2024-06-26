b811594ef5c1217e4a05288baf2c094d71077e89
galKatzav <galoosh1591@gmail.com>
2024-06-26 14:30:40 +0300

    IMenu Comments Update

--- interfaces/IMenu.py
+++ interfaces/IMenu.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod
 
+
+# Define the IMenu interface using the Abstract Base Class (ABC) module
 class IMenu(ABC):
     @abstractmethod
     def draw(self, screen):
+        """Draw the menu on the screen."""
         pass
 
     @abstractmethod
     def handle_event(self, event):
+        """Handle events such as keyboard input."""
         pass
 
     @abstractmethod
     def get_selected_difficulty(self):
+        """Get the currently selected difficulty level."""
         pass
