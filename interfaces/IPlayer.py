51cf4e11e292abd6a67f99b2a77fdc8fc67a67c3
galKatzav <galoosh1591@gmail.com>
2024-06-26 14:32:10 +0300

    IPlayer Comments Update

--- interfaces/IPlayer.py
+++ interfaces/IPlayer.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 
+
+# Define the IPlayer interface using the Abstract Base Class (ABC) module
 class IPlayer(ABC):
     @abstractmethod
     def move(self, dx, dy, maze, screen_width, screen_height):
+        """Move the player by (dx, dy) while checking collisions and bounds."""
         pass
 
     @abstractmethod
     def draw(self, screen):
+        """Draw the player on the screen."""
         pass
