06c86ef35e4e518c32629034c3a145fe4a3d849a
galKatzav <galoosh1591@gmail.com>
2024-06-26 14:33:10 +0300

    IGame Comment Update

--- interfaces/IGame.py
+++ interfaces/IGame.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
+
+# Define the IGame interface using the Abstract Base Class (ABC) module
 class IGame(ABC):
     @abstractmethod
     def run(self):
+        """Run the main game loop."""
         pass
